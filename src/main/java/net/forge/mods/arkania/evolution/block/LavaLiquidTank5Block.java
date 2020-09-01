bb7ae0b552e7d7a1e69dc34f5aa076e5abb49924
strykerzOfficiel <unconfigured@null.spigotmc.org>
2020-09-01 21:17:57 +0200

    Add Lava Liquid Tank

---
+++ src/main/java/net/forge/mods/arkania/evolution/block/LavaLiquidTank5Block.java
@@ -0,0 +1,47 @@
+
+package net.forge.mods.arkania.evolution.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.forge.mods.arkania.evolution.EvolutionModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@EvolutionModElements.ModElement.Tag
+public class LavaLiquidTank5Block extends EvolutionModElements.ModElement {
+	@ObjectHolder("evolution:lava_liquid_tank_5")
+	public static final Block block = null;
+	public LavaLiquidTank5Block(EvolutionModElements instance) {
+		super(instance, 97);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(null)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("lava_liquid_tank_5");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/net/forge/mods/arkania/evolution/procedures/LavaLiquidTankRetirerProcedure.java
@@ -0,0 +1,216 @@
+package net.forge.mods.arkania.evolution.procedures;
+
+import net.minecraftforge.items.ItemHandlerHelper;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.BlockState;
+
+import net.forge.mods.arkania.evolution.block.LavaLiquidTankBlock;
+import net.forge.mods.arkania.evolution.block.LavaLiquidTank9Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidTank7Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidTank6Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidTank5Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidTank3Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidTank2Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidTank10Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidBlock8Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidBlock4Block;
+import net.forge.mods.arkania.evolution.block.LavaLiquidBlock1Block;
+import net.forge.mods.arkania.evolution.EvolutionModElements;
+
+import java.util.Map;
+
+@EvolutionModElements.ModElement.Tag
+public class LavaLiquidTankRetirerProcedure extends EvolutionModElements.ModElement {
+	public LavaLiquidTankRetirerProcedure(EvolutionModElements instance) {
+		super(instance, 104);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure LavaLiquidTankRetirer!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure LavaLiquidTankRetirer!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure LavaLiquidTankRetirer!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure LavaLiquidTankRetirer!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure LavaLiquidTankRetirer!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == new ItemStack(Items.BUCKET, (int) (1)).getItem())) {
+			if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidTank10Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidTank9Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidTank9Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidBlock8Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidBlock8Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidTank7Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidTank7Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidTank6Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidTank6Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidTank5Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidTank5Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidBlock4Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidBlock4Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidTank3Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidTank3Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidTank2Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidTank2Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidBlock1Block.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == LavaLiquidBlock1Block.block.getDefaultState()
+					.getBlock())) {
+				{
+					BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+					BlockState _bs = LavaLiquidTankBlock.block.getDefaultState();
+					world.setBlockState(_bp, _bs, 3);
+				}
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.clearMatchingItems(p -> new ItemStack(Items.BUCKET, (int) (1)).getItem() == p.getItem(),
+							(int) 1);
+				if (entity instanceof PlayerEntity) {
+					ItemStack _setstack = new ItemStack(Items.LAVA_BUCKET, (int) (1));
+					_setstack.setCount((int) 1);
+					ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+				}
+			}
+		}
+	}
+}
